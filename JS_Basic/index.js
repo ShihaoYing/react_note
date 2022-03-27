8647e4fe9274b86542dd8ab96346a8c7349c8c29
ShihaoYing <skyingsky@gmail.com>
2022-03-26 21:21:54 -0700

    0346

--- JS_Basic/index.js
+++ JS_Basic/index.js
@@ -1,9 +1,32 @@
 // This is my first code!
-console.log('Hello World');
+// console.log('Hello World');
 
 
-let name = 'Shihao';
-console.log(name);
+// let name = 'Shihao';
+// console.log(name);
 
 // Variable can not be a reserved keyword
 // should be meaningful
+
+// let firstName = 'Shihao';
+// let lastName = 'Ying';
+
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// // constant
+// // TypeError: Assignment to constant variable
+// const interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+//Primitives/Value Types,  reference type
+let name = 'Mosh';  //string
+let age = 30; //number
+let isApproved = true; //boolean
+let firstName = undefined; 
+let selectColor = null;
+
+// dynamic typing
---
+++ JS_Basic/note.md
@@ -0,0 +1,21 @@
+Note
+==
+
+| Primitives/Value Types | Reference Type |
+| ---------------------- | -------------- |
+| String                 |                |
+| number                 |                |
+| boolean                |                |
+| undefined              |                |
+| null                   |                |
+
+    //Primitives/Value Types,  reference type
+    let name = 'Mosh';      //string
+    let age = 30;           //number
+    let isApproved = true;  //boolean
+    let firstName = undefined; 
+    let selectColor = null;
+
+
+
+
